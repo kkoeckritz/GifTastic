1d0bf8b187b7d0f284b64d6d5826d327326d6340
Kris Koeckritz <kjk464@humboldt.edu>
2018-07-12 14:13:37 -0700

    add displayButtons()

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,22 @@
-// get button onclick
-$("#cat-button").on("click", function() {
+var actors = [
+	"Jack Nicholson",
+	"Arnold Schwarzenegger",
+];
 
-    // giphy api endpoint
-    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cats";
+var gifs = {
+	displayButtons: function() {
+		for (a in actors) {
+			$("<button>", {
+				class: "unclicked",
+				id: `button_${a}`,
+				text: actors[a]
+			})
+			.appendTo("#app_buttons");
+		}
+	}
+}
 
-    // send request for ajax from giphy
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-
-    // with the response...
-      .then(function(response) {
-
-      // grab responded image URL
-        var imageUrl = response.data.image_original_url;
-
-        // create a new img element
-        var catImage = $("<img>");
-
-        // apply image URL to new img element
-        catImage.attr("src", imageUrl);
-        catImage.attr("alt", "cat image");
-
-        // prepend new img to page. Done. 
-        $("#images").prepend(catImage);
-      });
-  });
+$().ready(function() {
+	// DO IT.
+	gifs.displayButtons();
+})
